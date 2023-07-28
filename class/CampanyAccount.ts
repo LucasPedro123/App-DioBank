e3d795cf10dc7d5f7f0d5c3177abb1a1580cad12
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2023-07-28 20:25:44 -0300

    Criando class CampanyAccount-herdeira d'DioAccount

---
+++ class/CampanyAccount.ts
@@ -0,0 +1,8 @@
+import { DioAccount } from "./DioAccount";
+
+export class CampanyAccount extends DioAccount {
+
+    constructor(name: string, age: number, balance: number){
+        super(name, age,)
+    }
+}
